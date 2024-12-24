6e5f82fceafbb612a670feee98ef56cf1c182a51
snigdhaydv27 <snigdhak.ug23.cs@nitp.ac.in>
2024-12-24 18:19:04 +0530

    signup logic added in backend

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,7 +1,48 @@
-
+const User = require("../models/user.models");
 
 async function signup(req,res) {
-    res.send("Signup page");
+    try {
+        const {email,password,username} = req.body;
+
+        if(!email || !password || !username){
+            return res.status(400).json({success:false, message:"All fields are required"});
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({success:false, message:"Invalid Email"});
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({success:false, message:"Password must be atleast 6 characters"});
+        }
+
+        const existingUserByEmail=await User.findOne({email:email});
+        if(existingUserByEmail){
+            return res.status(400).json({success:false, message:"Email already exists"});
+        }
+
+        const existingUserByUsername=await User.findOne({username:username});
+        if(existingUserByUsername){
+            return res.status(400).json({success:false, message:"Username already exists"});
+        }
+        
+        const POFILE_PIC = ["/profile1.jpg","/profile2.jpg","/profile3.jpg"];
+        const image = POFILE_PIC[Math.floor(Math.random()*POFILE_PIC.length)];
+
+        const newUser=new User({
+             username:username,
+             email:email,
+             password:password,
+             image
+
+        });
+        await newUser.save();
+        
+     } catch (error) {
+        console.log("Error in signup controller: "+ error.message);
+        res.status(500).json({success:false, message:"Internal server error"});
+    }
 }
 async function login(req,res) {
     res.send("login page");
---
+++ backend/models/user.models.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+   username: {
+    type: String,
+    required: true,
+   },
+   email:{
+    type: String,
+    required: true,
+    unique: true,
+   },
+   password:{
+    type: String,
+    required: true,
+   },
+   image:{
+    type:String,
+    default: [],
+   },
+   searchHistory:{
+    type:Array,
+    default:[],
+   }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports=User;
+
+
+
+
+
+
