a10eea9f4e7ceefb78bb3156b7ee3d4d91058c33
snigdhaydv27 <snigdhak.ug23.cs@nitp.ac.in>
2024-12-25 11:19:10 +0530

    middleware  added

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,20 @@ const tvRoutes = require("./routes/tv.routes");
 
 const ENV_VAR = require("./config/envVar");
 const { connectDB } = require("./config/db");
+const cookieParser = require("cookie-parser");
+
+const middleRoute = require("./middleware/middleRoute");
 
 dotenv.config();
 const app = express();
 const PORT = ENV_VAR.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/movie",tvRoutes);
+app.use("/api/v1/movie",middleRoute, movieRoutes);
+app.use("/api/v1/movie",middleRoute,tvRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
