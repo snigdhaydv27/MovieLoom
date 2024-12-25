5be12158d3cd640e30ff82dd1df24756fbacf84c
snigdhaydv27 <snigdhak.ug23.cs@nitp.ac.in>
2024-12-25 19:31:52 +0530

    singup page designing started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import HomePage from './pages/home/homePage';
+import LoginPage from './pages/loginPage.jsx';
+import SignupPage from './pages/signupPage';
+// import SearchPage from './pages/searchPage';
+// import SearchHistoryPage from './pages/searchHistoryPage';
+// import NotFoundPage from './pages/notFoundPage';
+
+
 
 function App() {
 
   return (
-    <>
-      hii
-    </>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/signup' element={ <SignupPage/>}/>
+    </Routes>
   )
 }
 
