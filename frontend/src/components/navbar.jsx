2ed697baba5821565533fbe4e24ea65eca780f21
snigdhaydv27 <snigdhak.ug23.cs@nitp.ac.in>
2024-12-27 13:01:06 +0530

    homeScreen page designed and bugs fixed

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,83 +1,64 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { LogOut, Menu, Search } from "lucide-react";
+import  useAuthStore  from "../stores/authUser";
+import { useContentStore } from "../stores/content";
 
-const navbar = () => {
-  return (
-    <div>
-      <nav className="bg-gray-800">
-  <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div className="relative flex h-16 items-center justify-between">
-      <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-       
-        <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span className="absolute -inset-0.5"></span>
-          <span className="sr-only">Open main menu</span>
-         
-          <svg className="block size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-          
-          <svg className="hidden size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-      <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div className="flex shrink-0 items-center">
-          <img className="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"/>
-        </div>
-        <div className="hidden sm:ml-6 sm:block">
-          <div className="flex space-x-4">
-           
-            <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Dashboard</a>
-            <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-            <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-            <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
-          </div>
-        </div>
-      </div>
-      <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-          <span className="absolute -inset-1.5"></span>
-          <span className="sr-only">View notifications</span>
-          <svg className="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-          </svg>
-        </button>
+const Navbar = () => {
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+	const { user, logout } = useAuthStore();
 
-        <div className="relative ml-3">
-          <div>
-            <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-              <span className="absolute -inset-1.5"></span>
-              <span className="sr-only">Open user menu</span>
-              <img className="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-            </button>
-          </div>
+	const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
-    
-          <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-            
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+	const { setContentType } = useContentStore();
 
-  <div className="sm:hidden" id="mobile-menu">
-    <div className="space-y-1 px-2 pb-3 pt-2">
-     
-      <a href="#" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a>
-      <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-      <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-      <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
-    </div>
-  </div>
-</nav>
+	return (
+		<header className='max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20'>
+			<div className='flex items-center gap-10 z-50'>
+				<Link to='/'>
+					<img src='/logo2.png' alt='ZenG Logo' className='w-32 sm:w-40'/> 
+				</Link>
 
-    </div>
-  )
-}
+				{/* desktop navbar items */}
+				<div className='hidden sm:flex gap-4 items-center'>
+					<Link to='/' className='hover:underline' onClick={() => setContentType("movie")}>
+						Movies
+					</Link>
+					<Link to='/' className='hover:underline' onClick={() => setContentType("tv")}>
+						Tv Shows
+					</Link>
+					<Link to='/history' className='hover:underline'>
+						Search History
+					</Link>
+				</div>
+			</div>
 
-export default navbar
+			<div className='flex gap-3 items-center z-50'>
+				<Link to={"/search"}>
+					<Search className='size-6 cursor-pointer' />
+				</Link>
+				<img src={user.image} alt='Avatar' className='h-10 w-10 rounded cursor-pointer' />
+				<LogOut className='size-6 cursor-pointer' onClick={logout} />
+				<div className='sm:hidden'>
+					<Menu className='size-6 cursor-pointer' onClick={toggleMobileMenu} />
+				</div>
+			</div>
+
+			{/* mobile navbar items */}
+			{isMobileMenuOpen && (
+				<div className='w-full sm:hidden mt-4 z-50 bg-black border rounded border-gray-800'>
+					<Link to={"/"} className='block hover:underline p-2' onClick={toggleMobileMenu}>
+						Movies
+					</Link>
+					<Link to={"/"} className='block hover:underline p-2' onClick={toggleMobileMenu}>
+						Tv Shows
+					</Link>
+					<Link to={"/history"} className='block hover:underline p-2' onClick={toggleMobileMenu}>
+						Search History
+					</Link>
+				</div>
+			)}
+		</header>
+	);
+};
+export default Navbar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+import tailwindScrollbarHide from "tailwind-scrollbar-hide";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -18,5 +19,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindScrollbarHide],
 }
