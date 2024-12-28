40cab0d399dac80b294154a505a598217b7f0559
snigdhaydv27 <snigdhak.ug23.cs@nitp.ac.in>
2024-12-28 14:20:39 +0530

    vit config modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,8 @@ export default defineConfig({
 		},
 	},
 	resolve: {
-		extensions: ['.js', '.jsx', '.ts', '.tsx']
-	  }
-	  
+		alias: {
+		  '@': '/src', // Ensure this doesn't conflict
+		},
+	 },	    
 });
